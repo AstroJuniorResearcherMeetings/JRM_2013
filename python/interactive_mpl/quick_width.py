1c64603b5379e8c66fc41613b22c502a4c379668
Tim Anderton <quidditymaster@gmail.com>
2013-08-19 13:49:18 -0600

    quick_width measures equivalent widths

--- python/interactive_mpl/quick_width.py
+++ python/interactive_mpl/quick_width.py
@@ -4,12 +4,12 @@ import astropy.io.fits as fits
 import scipy.optimize
 import scipy.integrate
 
-def get_gaussian(self, wvs, center, g_width, g_depth, continuum):
+def get_gaussian(wvs, center, g_width, g_depth, continuum):
     return continuum*(1.0 - g_depth*np.exp(-0.5*(wvs-center/g_width)**2))
 
 class WidthMeasurement:
     
-    def __init__(self, center, left, right, g_width, g_depth
+    def __init__(self, center, left, right, g_width, g_depth,
                  continuum, integrated_width):
         self.left = left
         self.right = right
@@ -25,18 +25,23 @@ class WidthMeasurement:
 
     def __str__(self):
         out_tup = (self.center, self.left, self.right, self.g_width, self.continuum)
-        print "% 10.3f% 10.3f% 10.3f% 10.3f% 10.3f" % out_tup
+        return "% 10.3f% 10.3f% 10.3f% 10.3f% 10.3f" % out_tup
 
 
-class WidthFinder(wvs, flux):
+class WidthFinder:
     
     def __init__(self, wvs, flux):
         self.wvs = wvs
         self.flux = flux
         self.c_left = None
         self.c_right = None
-        self.vlines = plt.vline([wvs[0], wvs[0]], 0.5, 1.5)
-        self.fit_plot ,= plt.plot([wvs[0], wvs[1]], [1, 1])
+        self.fig = plt.figure()
+        self.ax = self.fig.add_subplot(111)
+        self.vlines = self.ax.vlines([wvs[0], wvs[0]], 0.5, 1.5)
+        self.fit_plot ,= self.ax.plot([wvs[0], wvs[1]], [1, 1])
+        self.flux_plot ,= self.ax.plot(wvs, flux)
+        self.canvas = self.fig.canvas
+        self.canvas.mpl_connect('key_press_event', self.on_key)
         self.c_measurement = None
         self.measurements = []
     
@@ -48,8 +53,8 @@ class WidthFinder(wvs, flux):
 
     def fit_gaussian(self):
         try:
-            assert c_left != None
-            assert c_right != None
+            assert self.c_left != None
+            assert self.c_right != None
             fit_mask = self.wvs >= self.c_left
             fit_mask *= (self.wvs <= self.c_right)
             masked_wvs = self.wvs[fit_mask]
@@ -70,8 +75,9 @@ class WidthFinder(wvs, flux):
             depth_guess = np.min(decr_sum)
             guess_vec = np.array([cent_guess, width_guess, depth_guess, cont_guess])
             #start the optimization running
-            fit_params = scipy.optimize.fmin(g_error, guess_vec, maxfun=10000)[0]
-            fcent, fwidth, fdepth, fcont = fit_params
+            fit_res = scipy.optimize.fmin(g_error, guess_vec, maxfun=10000)
+            #import pdb; pdb.set_trace()
+            fcent, fwidth, fdepth, fcont = fit_res
             #carry out simpsons rule integration between the left and right
             integrated_width = scipy.integrate.simps(fcont-masked_flux, masked_wvs)
             self.c_measurement = WidthMeasurement(fcent, self.c_left, self.c_right, 
@@ -98,4 +104,32 @@ class WidthFinder(wvs, flux):
 
 if __name__ == "__main__":
 
+    #load the fits file
+    hdul = hst_spec = fits.open("o5f607010_x1d.fits")
+    #select an order to fit
+    order_number = 4
+    
+    #use these commands interactively to figure out where the data we want is
+    #hdul.info()
+    #hdul[1].header.ascardlist()
+    
+    #the fits file is made up of "hdu's"
+    #that is Header Data Units
+    #the second hdu has our data
+    #third row is wavelength
+    print hdul
+    wvs = np.array(hdul[1].data["WAVELENGTH"])[order_number]
+    #seventh is flux
+    flux = np.array(hdul[1].data["FLUX"])[order_number]
+    
+    #super rough continuum normalization of data
+    fl_med = np.median(flux)
+    cont_est = np.median(flux[flux > fl_med])
+    flux /= cont_est
+
+    wf = WidthFinder(wvs, flux)
+    plt.show()
+    
+    
+    
     
